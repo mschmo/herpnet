b8b2caa754b6c0cfeb88a8261211154e9d3e861f
Matt <mschmoyer@50onred.com>
2013-10-03 19:42:56 -0400

    Simplifies query params for user using key words

--- herpnet.py
+++ herpnet.py
@@ -14,13 +14,26 @@ class Herpnet:
         self.api_key = api_key
 
         for endpoint in endpoints:
-            fun = self.__make_get_endpoint_fun(endpoint)
+            fun = self.__make_endpoint_fun(endpoint)
             setattr(self, endpoint.replace('/', '_'), fun)
 
     def __make_endpoint_fun(self, name):
-        def _function(options={}):
+        def _function(**kwargs):
+            options={}
+            options.update({
+                't': kwargs.get('taxon', ''),
+                'l': kwargs.get('location', ''),
+                'c': kwargs.get('code', ''),
+                'd': kwargs.get('date_range', ''),
+                'q': kwargs.get('other', ''),
+                'p': kwargs.get('geo', ''),
+                'm': kwargs.get('map', '')
+            })
             return self.get('{}/'.format(name), options)
         return _function
+
+    def _update_options(options, **kwargs):
+            options.update({'t': kwargs.get})
 
     def get(self, request, options):
         options.update({'api': self.api_key})
